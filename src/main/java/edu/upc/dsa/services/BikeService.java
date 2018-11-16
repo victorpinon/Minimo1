36da891639f1a64f9c7c4b7b0acf430dfd5c01b8
victorpinon <victorpinonrattia@hotmail.com>
2018-11-16 11:56:51 +0100

    solo falta el rest

--- src/main/java/edu/upc/dsa/services/BikeService.java
+++ src/main/java/edu/upc/dsa/services/BikeService.java
@@ -1,6 +1,5 @@
 package edu.upc.dsa.services;
 
-import edu.upc.dsa.Track;
 import edu.upc.dsa.MyBike;
 import edu.upc.dsa.MyBikeImpl;
 import io.swagger.annotations.Api;
@@ -41,71 +40,4 @@ public class BikeService {
 
 
     }
-
-    @Path("/")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getTracks() {
-
-
-        return Response.status(201).entity().build()  ;
-
-    }
-
-    @GET
-    @ApiOperation(value = "get a Track", notes = "asdasd")
-    @ApiResponses(value = {
-            @ApiResponse(code = 201, message = "Successful", response = Track.class),
-            @ApiResponse(code = 404, message = "Track not found")
-    })
-    @Path("/{id}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getTrack(@PathParam("id") int id) {
-        Track t = this.tm.getTrack(id);
-        if (t == null) return Response.status(404).build();
-        else  return Response.status(201).entity(t).build();
-    }
-
-    @DELETE
-    @ApiOperation(value = "delete a Track", notes = "asdasd")
-    @ApiResponses(value = {
-            @ApiResponse(code = 201, message = "Successful"),
-            @ApiResponse(code = 404, message = "Track not found")
-    })
-    @Path("/{id}")
-    public Response deleteTrack(@PathParam("id") int id) {
-        Track t = this.tm.getTrack(id);
-        if (t == null) return Response.status(404).build();
-        else this.tm.deleteTrack(id);
-        return Response.status(201).build();
-    }
-
-    @PUT
-    @ApiOperation(value = "update a Track", notes = "asdasd")
-    @ApiResponses(value = {
-            @ApiResponse(code = 201, message = "Successful"),
-            @ApiResponse(code = 404, message = "Track not found")
-    })
-    @Path("/")
-    public Response updateTrack(Track track) {
-        Track t = this.tm.getTrack(track.getId());
-        if (t == null) return Response.status(404).build();
-        else this.tm.updateTrack(t);
-        return Response.status(201).build();
-    }
-
-
-
-    @POST
-    @ApiOperation(value = "create a new Track", notes = "asdasd")
-    @ApiResponses(value = {
-            @ApiResponse(code = 201, message = "Successful", response=Track.class),
-    })
-
-    @Path("/")
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response newTrack(Track track) {
-        this.tm.addTrack(track);
-        return Response.status(201).entity(track).build();
-    }
-
 }
